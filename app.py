3ad528b84ee920287a4e4598182a406ab8ff61a3
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2024-03-08 15:46:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ st.sidebar.info(
 )
 
 st.sidebar.title("Contact")
-st.sidebar.info(
-    """
+'''st.sidebar.info(
+    
     Qiusheng Wu: <https://wetlands.io>
     [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
-    """
-)
+    
+)'''
 
 st.title("Streamlit for Geospatial Applications")
 
