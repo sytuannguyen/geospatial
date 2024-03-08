2fae744592835926adf305d0d84a6b5ec9c7b34b
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2024-03-08 16:32:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.sidebar.info(
 st.sidebar.title("Contact")
 st.sidebar.info(
     """
-    Qiusheng Wu: <https://wetlands.io>
+    app.py
     [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
     """
 )
